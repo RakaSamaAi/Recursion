bd99efae5cfcf6fba6b3a8d29161b7a7b7f780b1
Dimase <zwere4ek@list.ru>
2024-02-04 03:21:21 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    static void Main(string[] args)
+    {
+        int m = 2; 
+        int n = 1; 
+
+        Console.WriteLine($"Ackermann({m}, {n}) = {Ackermann(m, n)}");
+    }
+}
