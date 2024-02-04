e8ed343e4f0e49e41b7dbd1258f2e526f78dba4b
Dimase <zwere4ek@list.ru>
2024-02-04 03:20:56 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int M = 1;
+        int N = 10;
+
+        PrintNumbersInRange(M, N);
+    }
+
+    static void PrintNumbersInRange(int start, int end)
+    {
+        if (start > end)
+        {
+            return;
+        }
+        Console.WriteLine(start);
+        PrintNumbersInRange(start + 1, end);
+    }
+}
