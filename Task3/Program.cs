c796864baaefe46b35c99caa1d7e09c6f763bed7
Dimase <zwere4ek@list.ru>
2024-02-04 03:21:37 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index < 0 || index >= arr.Length)
+            return;
+
+        Console.Write(arr[index] + " ");
+
+        PrintArrayReversed(arr, index - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+
+        PrintArrayReversed(array, array.Length - 1);
+    }
+}
